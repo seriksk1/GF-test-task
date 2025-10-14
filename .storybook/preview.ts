8ad12cc0c01171564cffdd17c40ce003ab364bcc
seriksk1 <seriksk1@ukr.net>
2025-10-15 00:06:44 +0300

    Feature: Create the story for the Button component

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -3,7 +3,6 @@ import type { Preview } from '@storybook/nextjs-vite';
 
 const preview: Preview = {
   parameters: {
-    actions: { argTypesRegex: '^on[A-Z].*' },
     controls: {
       expanded: true,
       matchers: {
